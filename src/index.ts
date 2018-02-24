54fd0d3eab486cff6c62b4cef91339aea92e3722
Riku Rouvila <riku.rouvila@gmail.com>
2018-02-24 15:42:38 +0000

    add websocket service for face detection

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
+import * as http from "http";
+import { promisify } from "util";
+import { extname } from "path";
+import * as cv from "opencv4nodejs";
+import * as WS from "ws";
 import * as AWS from "aws-sdk";
 import * as express from "express";
 import * as multer from "multer";
-import { promisify } from "util";
-import { extname } from "path";
 import * as cors from "cors";
 import * as faceapp from "faceapp";
 
-const bodyParser = multer();
 const app = express();
+app.use(cors());
+const bodyParser = multer();
 
 const rekognition = new AWS.Rekognition({
   region: "eu-west-1",
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY
 });
+
 const s3 = new AWS.S3({
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY
@@ -25,9 +30,6 @@ const indexFaces = promisify(rekognition.indexFaces.bind(rekognition));
 const searchFacesByImage = promisify(
   rekognition.searchFacesByImage.bind(rekognition)
 );
-
-app.use(cors());
-app.use(bodyParser.single("file"));
 
 const UPLOAD_OPTIONS = { partSize: 10 * 1024 * 1024, queueSize: 1 };
 const BUCKET_NAME = process.env.BUCKET_NAME || "";
@@ -44,7 +46,7 @@ async function uploadFile(file: Express.Multer.File) {
   return filename;
 }
 
-app.post("/faces", async (req, res) => {
+app.post("/faces", bodyParser.single("file"), async (req, res) => {
   if (!req.body.id) {
     return res.status(400).send('Missing "id" field');
   }
@@ -70,7 +72,7 @@ app.post("/faces", async (req, res) => {
   res.status(201).send();
 });
 
-app.post("/recognize", async (req, res) => {
+app.post("/recognize", bodyParser.single("file"), async (req, res) => {
   try {
     const filename = await uploadFile(req.file);
     const result = await searchFacesByImage({
@@ -98,7 +100,7 @@ app.post("/recognize", async (req, res) => {
   }
 });
 
-app.post("/transform", async (req, res) => {
+app.post("/transform", bodyParser.single("file"), async (req, res) => {
   try {
     const buffer = await faceapp.process(
       req.file.buffer,
@@ -111,4 +113,38 @@ app.post("/transform", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000);
+const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
+
+const server = http.createServer(app);
+const wss = new WS.Server({ server });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", () => {});
+  ws.on("message", async function incoming(message: string) {
+    const data = JSON.parse(message);
+
+    const { id, image } = data;
+
+    const buffer = Buffer.from(
+      image.replace("data:image/png;base64,", ""),
+      "base64"
+    );
+    try {
+      const frame = cv.imdecode(buffer);
+      const grayImg = await frame.bgrToGrayAsync();
+      const objects = classifier.detectMultiScaleGpu(grayImg, {
+        scaleFactor: 1.1,
+        minNeighbors: 10
+      });
+      if (ws.OPEN) {
+        ws.send(JSON.stringify({ objects, id }));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
+
+server.listen(process.env.PORT || 3000, () =>
+  console.log("Listening on %d", server.address().port)
+);
