9e8b901da694ce0660c257656434b438f403f8e5
Minna Niemi <minna.niemi@futurice.com>
2018-03-04 10:15:27 +0000

    filter list of suspects by tribe
    uses locally stored version of list of people, API endpoint not yet implemented

--- src/services/people.ts
+++ src/services/people.ts
@@ -1,3 +1,26 @@
-export function getPeople() {
-  return { data: false };
+const RETURNED_NAMES_AMOUNT = 6
+
+export interface Person {
+  username: string 
+  first: string 
+  last: string 
+  team: string 
+  competence: string
+  supervisor: string 
+  supervisorName: string 
+  start: string 
+  active: string 
 }
+
+export function getPeople() {
+  const peopleList: Person[] = require('./peopleList.json');
+
+  return peopleList
+    .filter(byTribe('London')) // Hardcoding ftw
+    .map(getFullName)
+    .slice(0, RETURNED_NAMES_AMOUNT);
+}
+
+const getFullName = (p: Person) => `${p.first} ${p.last}`
+
+const byTribe = (tribeName: string) => (p: Person) => p.team.includes(tribeName)
